4d7c2148733a89417e80a69441718a808dce03a1
Carlton Schuyler <cschuyle@Carltons-Mac-mini.local>
2024-05-12 13:09:52 -0600

    Lots of extra brew stuff for cschuyle

---
+++ scripts/opt-in/cschuyle.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+echo
+echo "Installing Carlton's stuff"
+
+brew install --cask brave-browser
+brew install --cask divvy
+brew install --cask omnidb
+brew install --cask roam-research
+brew install --cask vlc
+# brew install --cask vscodium
+# brew install amz
+brew install automake
+brew install bcrypt
+brew install brimdata/tap/zq
+brew install clojure
+brew install clojure/tools/clojure
+brew install leiningen
+brew install coreutils
+brew install cpanm
+brew install csvkit
+brew install curl
+# brew install dg
+brew install direnv
+brew install findutils # gfind etc
+# brew install firebase
+brew install gh
+brew install git
+brew tap gdubw/gng && brew install gng
+brew install gnu-sed # gsed
+brew install gnu-tar # gtar
+brew install gradle
+brew install handbrake
+brew tap heroku/brew && brew install heroku
+brew install homebrew/cask/emacs
+brew install httpie
+brew install imagemagick
+brew install jq
+brew install julia
+brew install kubernetes-cli
+brew install libdvdcss
+brew install libidn2
+brew install libpq
+brew install link-grammar
+brew install maven
+brew install meld
+brew install mk-configure
+brew install mkvtools
+brew install node
+brew install nushell
+brew install openssl
+brew install pdfgrep
+# brew install pdftk
+# brew install pdftk-java
+brew install perl
+brew install pkgxdev/made/pkgx
+brew install podman
+brew install postgresql
+# brew install psql
+brew install pup
+brew install python
+brew install readline
+brew install rename
+brew install ruby
+brew install s3cmd
+brew install sqlite3
+brew install the_silver_searcher
+brew install thefuck
+brew install tre # for agrep. https://github.com/laurikari/tre
+brew install tree
+brew install vlc
+brew install vmware-tanzu-learning/tap/penguinctl
+brew install watch 
+brew install WebPQuickLook
+brew install wget
+brew install xmlstarlet
+brew install xz
+brew install yarn
+brew install zlib
+brew install zq
+brew install zsh
