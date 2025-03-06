df5270dcbbc96fccc55ade49efeaab0b9fb124d8
Carlton Schuyler <carl@dragnon.com>
2025-03-05 17:11:05 -0700

    very rough linux opt-in for cschuyle:

---
+++ scripts/opt-in/cschuyle.linux.sh
@@ -0,0 +1,13 @@
+sudo apt install silversearcher-ag
+sudo apt install flatpak
+sudo apt install libdvdcss2
+sudo apt install libdvd-pkg && sudo dpkg-reconfigure libdvd-pkg
+sudo apt install net-tools
+#sudo apt install ./synology-assistant_7.0.5-50070_amd64.deb
+#sudo apt install ./code_1.97.2-1739406807_amd64.deb
+sudo apt install imagemagick
+sudo apt install perl-doc
+sudo apt install libfuse2
+sudo apt-get install csvkit
+#sudo apt install ./naps2-7.5.3-linux-x64.deb
+sudo apt install copyq
